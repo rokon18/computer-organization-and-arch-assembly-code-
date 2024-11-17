d89824427dd3ce654d821540d4b86301cb940058
MD. ROKONUZZAMAN <rokonkhan1819@gmail.com>
2024-11-17 22:54:01 +0600

    computer organization and arch course assembly code case conversion

---
+++ case conversion.asm
@@ -0,0 +1,42 @@
+;TITLE PGM4_2: PRINT STRING PROGRAM   
+
+
+.MODEL SMALL 
+.STACK 100H
+
+.DATA
+MSG1 DB 0DH,0AH, 'ENTER A LOWER CASE LETTER :$'; 
+MSG2 DB 0DH,0AH, 'ITS IS IN UPPER CASE :$';
+char DB ?,'$';
+
+
+.CODE 
+    MAIN PROC
+        ;initialize DS
+            MOV AX, @DATA
+            MOV DS, AX          ; initialize DS
+        ;display message
+            LEA DX, MSG1         ;get message
+            MOV AH, 9           ;disply string function
+            INT 21h             ;display message    
+            
+        ;input a character
+        MOV AH,1;
+        int 21H;
+        ;convert to upper case
+        SUB AL,20h;
+        mov CHAR,AL;
+        ;display on the next line  
+         LEA DX, MSG2;         ;get message
+            MOV AH, 9           ;disply string function
+            INT 21h 
+            
+             LEA DX, CHAR         ;get message
+            MOV AH, 9           ;disply string function
+            INT 21h 
+        
+        ;return to DOS
+            MOV AH, 4CH
+            INT 21h             ;DOS exit
+    MAIN ENDP
+    END MAIN
